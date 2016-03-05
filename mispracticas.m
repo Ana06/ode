be73caac78dfb54a41a71e3136d3f389932b8675
Ana María Martínez Gómez <anamma06@gmail.com>
2016-03-05 17:14:38 +0100

    Auxiliar modules
    To run tests with the methods implementations.

---
+++ mispracticas.m
@@ -0,0 +1,8 @@
+% INITIAL VALUE PROBLEMS
+
+% Cardioid
+% fun=@funccorazon; x0 = [0; 2]; tinic = 0; tfin = 2*pi; N = 80; par=[];
+
+% Van der Pol oscillator 
+% fun = @funcvanderpol; x0=[0.1;0.2]; tinic=0; tfin=10; N=1000; par=[]; jac=@jacvanderpol; itmax=10;
+
